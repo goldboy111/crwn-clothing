a78fe168f74ebcaedbb7c1999adbddb67311a7b5
goldboy111 <goldboy6060@gmail.com>
2022-06-17 00:24:11 +0200

    update our menu-items to navigate wuthRouterHoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
-import HomePage from './../src/pages/homepage.component.jsx'
+import HomePage from './../src/pages/homepage.component.jsx';
+import { Route, Router, Routes, Link } from 'react-router-dom';
+
+
+const HatsPage = (props)=>{
+  console.log(props);
+return(<div>
+  <h1>Hats Page</h1>
+</div>
+)
+  }
+
+const DetailsPage = props=>(
+    
+    
+   
+    
+    <div>
+      <h1>Details Page</h1>
+      
+    </div>
+    
+  )
 
 function App() {
   return (
     <div >
-      <HomePage />
+      
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
+      
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,35 +12,35 @@ class Directory extends Component {
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
                   size:'large',
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
                   size:'large',
-                  linkUrl: 'shop/jackets'
+                  linkUrl: ''
                 },
                 {
                   title: 'sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
                   size:'large',
-                  linkUrl: 'shop/sneakers'
+                  linkUrl: ''
                 },
                 {
                   title: 'womens',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens'
+                  linkUrl: ''
                 },
                 {
                   title: 'mens',
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: ''
                 }
               ]
               
@@ -53,7 +53,8 @@ render(){
             <div className="directory-menu">
                 {
                     this.state.sections.map(
-                        ({id,title,imageUrl,size})=> <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />   
+                        ({id,...otherSectionProps})=> 
+                        <MenuItem key={id} {...otherSectionProps}   />   
                         )
                 }
             </div>
